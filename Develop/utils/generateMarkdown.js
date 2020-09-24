5d003dbf930b5de99d2bb5d7a1600f42d4eb26df
PureDemon <umaga1031@yahoo.com>
2020-09-23 20:13:42 -0500

    gen complete

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,35 @@
+function generateMarkdown(data) {
+  return `
+# Project Title : ${data.title}
+## Project Description:
+${data.desc}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Test](#test)
+* [Questions](#questions)
+* [License](#license)
+* [Author] (#Author)
+* [Badges](#badges)
+## Installation
+${data.install}
+## Usage
+${data.usage}
+## Contributors
+${data.contributors}
+## Test
+${data.test}
+## Questions
+If you have any questions, contact ${data.username} on GitHub.
+## License
+MIT License
+Copyright (c) [year] [fullname]
+## Author 
+![GitHub profile pic](${data.image})
+## Badges
+![badmath](https://img.shields.io/github/repo-size/${data.username}/${data.repo})
+`;
+}
+
+module.exports = generateMarkdown;
